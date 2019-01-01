1abb23cf1189186e1ce6296f5132b7d55bf2e990
imsamnang <applephagna@gmail.com>
2019-01-02 00:56:46 +0700

    Save Product Item

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
@@ -9,6 +10,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+  public function __construct()
+  {
+    $this->middleware('auth:api')->except('index','show');
+  }
 
   public function index()
   {
@@ -20,9 +26,19 @@ class ProductController extends Controller
       //
   }
 
-  public function store(Request $request)
+  public function store(ProductRequest $request)
   {
-      //
+    $product = new Product();
+    $product->name = $request->name;
+    $product->detail = $request->description;
+    $product->price = $request->price;
+    $product->stock = $request->stock;
+    $product->discount = $request->discount;
+    $product->save();
+
+    return response([
+      'data' => new ProductResource($product)
+    ],201);
   }
 
   public function show(Product $product)
