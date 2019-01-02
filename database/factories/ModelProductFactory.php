a4d76fd0b3c689b06d7373a39bac187b05222228
imsamnang <applephagna@gmail.com>
2019-01-02 14:28:14 +0700

    Product Destroy

--- database/factories/ModelProductFactory.php
+++ database/factories/ModelProductFactory.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Product::class, function (Faker $faker) {
   return [
+  	'user_id' => function(){
+  		return User::all()->random();
+  	},  	
    	'name' => $faker->word,
    	'detail' => $faker->paragraph, 
    	'price' => $faker->numberBetween(100,1500),
